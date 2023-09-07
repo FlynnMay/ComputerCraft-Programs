e5f40ca681dcb60e2214d912b14e887ab9f9179e
FlynnMay <77155011+FlynnMay@users.noreply.github.com>
2023-09-07 19:38:06 +1000

    Update update.lua

--- update.lua
+++ update.lua
@@ -1,8 +1,8 @@
 local function loadFile(name)
-    shell.run("delete", name + ".lua")
+    shell.run("delete", name .. ".lua")
     os.sleep(.4)
-    shell.run("wget", "https://raw.githubusercontent.com/FlynnMay/ComputerCraft-Programs/master/" + name + ".lua")
-    print("Updated: " + name)
+    shell.run("wget", "https://raw.githubusercontent.com/FlynnMay/ComputerCraft-Programs/master/" .. name .. ".lua")
+    print("Updated: " .. name)
 end
 
 loadFile("quarry")
