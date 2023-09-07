9acba46d3ce3536ac3e48d5a9c3641ef6c28af5f
FlynnMay <77155011+FlynnMay@users.noreply.github.com>
2023-09-07 19:19:43 +1000

    Update update.lua

--- update.lua
+++ update.lua
@@ -5,4 +5,5 @@ function load(name)
     shell.run("delete", name)
     os.sleep(.4)
     shell.run("wget", "https://raw.githubusercontent.com/FlynnMay/ComputerCraft-Programs/master/" + name + ".lua")
+    print("Updated: " + name)
 end
