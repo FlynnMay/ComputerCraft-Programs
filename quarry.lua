624a7201a7af7de6e75b64955cce7cb8dc5af5f1
FlynnMay <77155011+FlynnMay@users.noreply.github.com>
2023-09-07 19:39:43 +1000

    Update quarry.lua

--- quarry.lua
+++ quarry.lua
@@ -1,1 +1,47 @@
-print("Test");
+
+if #arg < 2 then 
+    print("Usage: quarry <width> <length> <depth>");
+end
+
+local w = tonumber(arg[1]);
+local l = tonumber(arg[2]);
+local d = tonumber(arg[3]);
+
+local dirToggle = true;
+
+for i = 1, d, 1 do
+    for j = 1, l, 1 do
+        for k = 2, w, 1 do
+            turtle.dig();
+            turtle.forward();
+            -- write("F ")
+        end
+
+        if j ~= l then 
+            
+            if dirToggle then
+                turtle.turnRight()
+                turtle.dig()
+                turtle.forward()
+                turtle.turnRight()
+                -- write("R F R ")
+
+            else
+                turtle.turnLeft()
+                turtle.dig()
+                turtle.forward()
+                turtle.turnLeft()
+                -- write("L F L ")
+
+            end
+
+            dirToggle = not dirToggle;
+        end
+    end
+    turtle.digDown()
+    turtle.down()
+    turtle.turnRight()
+    turtle.turnRight()
+    -- write("D 180 \n")
+
+end
