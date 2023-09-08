56ce4236075a7f2bd75c76818f607e01c1748d46
FlynnMay <77155011+FlynnMay@users.noreply.github.com>
2023-09-08 20:39:43 +1000

    travel actions

--- travel.lua
+++ travel.lua
@@ -9,7 +9,7 @@ local function calibrateHeading()
     local locEnding = vector.new(gps.locate(2, false))
 
     local heading = locEnding - locOrigin
-    
+
     --[[
         -x = 1
         -z = 2
@@ -18,6 +18,58 @@ local function calibrateHeading()
     --]]
 
     return ((heading.x + math.abs(heading.x) * 2) + (heading.z + math.abs(heading.z) * 3))
+end
+
+local function sortCoordinatesDescending(vec)
+    local coordinatePairs = { { "x", vec.x }, { "y", vec.y }, { "z", vec.z } }
+    table.sort(coordinatePairs, function (a, b)
+        return a[2] > b[2]
+    end)
+
+    return coordinatePairs
+end
+
+-- local function wrap(value, min, max)
+--     if value > max then
+--         return wrap(max - value, min, max)
+--         elseif value < min then
+--             return
+--     end 
+    
+
+-- end
+
+local function buildTravelActions(heading, distances)
+    local lastHeading = heading
+    local actions = {}
+
+    local headingDecodeMap = {}
+    headingDecodeMap[1] = "-x"
+    headingDecodeMap[2] = "-z"
+    headingDecodeMap[3] = "+x"
+    headingDecodeMap[4] = "+z"
+
+    local headingEncodeMap = {}
+    headingEncodeMap["-x"] = 1
+    headingEncodeMap["-z"] = 2
+    headingEncodeMap["+x"] = 3
+    headingEncodeMap["+z"] = 4
+
+    for _, axisPair in ipairs(distances) do
+        local axisName = axisPair[1]
+        local length = axisPair[2]
+        local axisHeading = (length < 0 and "-" or "+") .. axisName
+        
+        -- if axisHeading ~= headingDecodeMap[lastHeading] then
+            for i = 1,  headingEncodeMap[axisHeading] % lastHeading do
+                table.insert(actions, turtle.turnRight)
+            end
+
+            for i = 1, length do
+                table.insert(actions, turtle.forward)
+            end
+        -- end
+    end
 end
 
 -- Main Code --
@@ -29,4 +81,10 @@ local pos = gps.locate()
 
 local target = vector.new(tonumber(arg[1]), tonumber(arg[2]), tonumber(arg[3]))
 
-print(calibrateHeading())
+local travelDistances = sortCoordinatesDescending(target - pos);
+
+local actions = buildTravelActions(calibrateHeading(), travelDistances)
+
+for _, action in ipairs(actions) do
+    action()
+end
