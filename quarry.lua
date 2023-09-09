47d21ed3375ae43e269a7677bbf25f3104d1814b
FlynnMay <77155011+FlynnMay@users.noreply.github.com>
2023-09-10 02:06:00 +1000

    .

--- quarry.lua
+++ quarry.lua
@@ -3,38 +3,35 @@ if #arg < 6 then
     print("Usage: quarry size: <width> <length> <depth> position: <x> <y> <z>");
 end
 
+local extendedTurtle = require("extendedTurtle")
 
 local w = tonumber(arg[1]);
 local l = tonumber(arg[2]);
 local d = tonumber(arg[3]);
 local startPoint = vector.new(tonumber(arg[4]), tonumber(arg[5]), tonumber(arg[6]))
 
-shell.run("travel.lua",   startPoint.x .. " " .. startPoint.y .. " " .. startPoint.z)
+extendedTurtle.moveTo(startPoint)
 
 local dirToggle = true;
 
 for i = 1, d, 1 do
     for j = 1, l, 1 do
         for k = 2, w, 1 do
-            turtle.dig();
-            turtle.forward();
-            -- write("F ")
+            extendedTurtle.forward();
         end
 
         if j ~= l then 
             
             if dirToggle then
-                turtle.turnRight()
-                turtle.dig()
-                turtle.forward()
-                turtle.turnRight()
+                extendedTurtle.turnRight();
+                extendedTurtle.forward()
+                extendedTurtle.turnRight()
                 -- write("R F R ")
 
             else
-                turtle.turnLeft()
-                turtle.dig()
-                turtle.forward()
-                turtle.turnLeft()
+                extendedTurtle.turnLeft()
+                extendedTurtle.forward()
+                extendedTurtle.turnLeft()
                 -- write("L F L ")
 
             end
@@ -42,10 +39,9 @@ for i = 1, d, 1 do
             dirToggle = not dirToggle;
         end
     end
-    turtle.digDown()
-    turtle.down()
-    turtle.turnRight()
-    turtle.turnRight()
+    extendedTurtle.down()
+    extendedTurtle.turnRight()
+    extendedTurtle.turnRight()
     -- write("D 180 \n")
 
 end
