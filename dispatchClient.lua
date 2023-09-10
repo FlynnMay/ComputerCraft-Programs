8d76dcc4c3aa9404ebe0e027cd8dc7868785a6b2
FlynnMay <77155011+FlynnMay@users.noreply.github.com>
2023-09-10 23:07:10 +1000

    .

--- dispatchClient.lua
+++ dispatchClient.lua
@@ -4,7 +4,7 @@ local CLIENT_PORT = 18
 local modem = peripheral.wrap("left")
 modem.open(CLIENT_PORT)
 
-modem.transmit(SERVER_PORT, CLIENT_PORT, "CLIENT_DEPLOYED")
+modem.transmit(SERVER_PORT, CLIENT_PORT, "Dispatched_Connection_Established")
 local event, side, senderChannel, replyChannel, msg, distance = os.pullEvent("modem_message")
 
 shell.run("quarry", msg)
