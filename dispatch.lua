c3722b822a18c48c1f9ff4c9d1e43573ec10981f
FlynnMay <77155011+FlynnMay@users.noreply.github.com>
2023-09-11 00:12:35 +1000

    .

--- dispatch.lua
+++ dispatch.lua
@@ -133,6 +133,5 @@ end
 
 for i = 1, #subRectangles do
     local subRectangle = subRectangles[i]
-    write(subRectangle.x .. " " .. pos.y .. " " .. subRectangle.z)
-    deploy(vector.new(subRectangle.x, pos.y, subRectangle.z), w, l, d)
+    deploy(vector.new(subRectangle["x"], pos.y, subRectangle["z"]), w, l, d)
 end
