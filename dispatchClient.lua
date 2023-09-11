bad3385625bcd01f346e554d3cda84363bb24782
FlynnMay <77155011+FlynnMay@users.noreply.github.com>
2023-09-11 12:47:11 +1000

    .

--- dispatchClient.lua
+++ dispatchClient.lua
@@ -1,7 +1,22 @@
 local SERVER_PORT = 1997
 local CLIENT_PORT = 18
  
-local modem = peripheral.wrap("left")
+local function findModem()
+    local sides = {"top", "bottom", "front", "back", "left", "right"}
+
+    for _, side in pairs(sides) do
+        if peripheral.isPresent(side) and peripheral.getType(side) == "modem" then
+            local modem = peripheral.wrap(side)
+            if modem.isWireless() then
+                return modem;    
+            end
+        end
+        
+    end
+end
+
+local modem = findModem()
+
 modem.open(CLIENT_PORT)
 
 modem.transmit(SERVER_PORT, CLIENT_PORT, "Dispatched_Connection_Established")
