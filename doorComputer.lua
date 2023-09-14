e2d8db5bd27414f92b6a7ebdf5e22855afdde866
FlynnMay <77155011+FlynnMay@users.noreply.github.com>
2023-09-14 18:35:27 +1000

    .

--- doorComputer.lua
+++ doorComputer.lua
@@ -1,13 +1,35 @@
-rednet.open("top")
+function string.split(inputString, delimiter)
+    local substrings = {}
+    local pattern = "[^" .. delimiter .. "]+"
 
+    for match in inputString:gmatch(pattern) do
+        table.insert(substrings, match)
+    end
+
+    return substrings
+end
+
+function stringTableToVector(strTable)
+    return vector.new(tonumber(strTable[1]), tonumber(strTable[2]), tonumber(strTable[3]))
+end
+
+
+-- Main Code --
+if #arg < 1 then
+    print("Usage: doorComputer <open threshold> <power output dir>")
+end
+
+local openThreshold = tonumber(arg[1])
+local outputDir = arg[2]
+
+rednet.open("top")
+local pos = vector.new(gps.locate())
 
 while true do
     local senderID, message, protocol = rednet.receive()
-    redstone.setOutput("bottom", true)
-
-    if protocol == "location" then
-        print("Location: " .. message)
-        redstone.setOutput("bottom", false)
-        print("door found")
-    end
+    local arguments = string.split(message, ' ')
+    local clientPos = stringTableToVector(arguments)
+    local distance = vector.length(pos - clientPos)
+    
+    redstone.setOutput(outputDir, distance <= openThreshold)
 end
